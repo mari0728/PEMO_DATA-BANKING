3e96d0c7e9eb5f8501a3b655387cab9b5e359a5c
mari0728 <gmhmmorales0728@gmail.com>
2024-07-12 15:51:24 +0800

    operators final

--- Models/PEMO.Designer.cs
+++ Models/PEMO.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\joshu\OneDrive\Desktop\ojt\7 4 2024\PEMO_DATA-BANKING\Models\PEMO.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Gebruiker\Documents\gem\PEMO_DATA-BANKING\Models\PEMO.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
